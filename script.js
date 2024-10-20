4c346b33f07e3671803eacb06771dc83c622d2c8
SUYAB123 <111383970+SUYAB123@users.noreply.github.com>
2024-10-20 20:21:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  employees.map(employee => {
+        if (employee.profession === "developer") {
+            console.log(employee);
+        }
+    });
 }
-
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  employees.forEach(employee => {
+        if (employee.profession === "developer") {
+            console.log(employee);
+        }
+    });
 }
 
 function addData() {
-  //Write your code here, just console.log
+ const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    employees.push(newEmployee);
+    console.log(employees);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+ const filteredEmployees = employees.filter(employee => employee.profession !== "admin");
+    console.log(filteredEmployees);
 }
-
 function concatenateArray() {
-  //Write your code here, just console.log
+ console.log(concatenatedArray);
 }
